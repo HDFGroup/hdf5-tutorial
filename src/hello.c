fb0da64f2c6b21de319c50ab7b7c10af33c87003
Gerd Heber <gheber@hdfgroup.org>
2024-01-05 14:20:19 +0000

    Introduce the concept of arguments.

--- src/hello.c
+++ src/hello.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
     printf("Hello, World!\n");
+
+    if (argc > 1)
+    {
+        printf("Arguments:\n");
+        for (int i = 0; i < argc; ++i)
+            printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    else
+        printf("No arguments\n");
+
     return 0;
 }
