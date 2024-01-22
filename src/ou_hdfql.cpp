db850e1e47c4171039763de3786f4e98b76ec476
Gerd Heber <gheber@hdfgroup.org>
2024-01-22 06:19:06 -0600

    Updates/2024 01 24 (#56)
    * Add the HDFql source file.
    
    * Added the missing attributes.

--- src/ou_hdfql.cpp
+++ src/ou_hdfql.cpp
@@ -25,24 +25,22 @@ int main()
     // Write the sample paths to an HDF5 file using the HDFql C++ bindings!
     //
 
-    HDFql::execute("CREATE TRUNCATE FILE ou_hdfql.h5");
-    HDFql::execute("CREATE ATTRIBUTE ou_hdfql.h5 source AS VARCHAR VALUES(\"https://github.com/HDFGroup/hdf5-tutorial\")");
+    HDFql::execute("CREATE TRUNCATE AND USE FILE ou_hdfql.h5");
+    HDFql::execute("CREATE ATTRIBUTE source AS VARCHAR VALUES(\"https://github.com/HDFGroup/hdf5-tutorial\")");
 
     ostringstream query;
-    HDFql::execute(sstr("CREATE DATASET ou_hdfql.h5 \"dataset\" AS DOUBLE(" << path_count << ", " << step_count << ")"));
+    HDFql::execute(sstr("CREATE DATASET \"dataset\" AS DOUBLE(" << path_count << ", " << step_count << ") VALUES FROM MEMORY " << HDFql::variableTransientRegister(ou_process)));
 
-    HDFql::variableRegister(ou_process.data());
-    HDFql::execute(sstr("INSERT INTO ou_hdfql.h5 \"dataset\" VALUES FROM MEMORY " << HDFql::variableGetNumber(ou_process.data())));
-    HDFql::variableUnregister(ou_process.data());
-    
-    HDFql::execute("CREATE ATTRIBUTE ou_hdfql.h5 dataset/comment AS VARCHAR VALUES(\"This dataset contains sample paths of an Ornstein-Uhlenbeck process.\")");
-    HDFql::execute("CREATE ATTRIBUTE ou_hdfql.h5 dataset/Wikipedia AS VARCHAR VALUES(\"https://en.wikipedia.org/wiki/Ornstein%E2%80%93Uhlenbeck_process\")");
-    HDFql::execute("CREATE ATTRIBUTE ou_hdfql.h5 dataset/rows AS VARCHAR VALUES(\"path\")");
-    HDFql::execute("CREATE ATTRIBUTE ou_hdfql.h5 dataset/columns AS VARCHAR VALUES(\"time\")");
-    HDFql::execute(sstr("CREATE ATTRIBUTE ou_hdfql.h5 dataset/dt AS DOUBLE VALUES(" << dt << ")"));
-    HDFql::execute(sstr("CREATE ATTRIBUTE ou_hdfql.h5 dataset/θ AS DOUBLE VALUES(" << theta << ")"));
-    HDFql::execute(sstr("CREATE ATTRIBUTE ou_hdfql.h5 dataset/μ AS DOUBLE VALUES(" << mu << ")"));
-    HDFql::execute(sstr("CREATE ATTRIBUTE ou_hdfql.h5 dataset/σ AS DOUBLE VALUES(" << sigma << ")"));
+    HDFql::execute("CREATE ATTRIBUTE dataset/comment AS VARCHAR VALUES(\"This dataset contains sample paths of an Ornstein-Uhlenbeck process.\")");
+    HDFql::execute("CREATE ATTRIBUTE dataset/Wikipedia AS VARCHAR VALUES(\"https://en.wikipedia.org/wiki/Ornstein%E2%80%93Uhlenbeck_process\")");
+    HDFql::execute("CREATE ATTRIBUTE dataset/rows AS VARCHAR VALUES(\"path\")");
+    HDFql::execute("CREATE ATTRIBUTE dataset/columns AS VARCHAR VALUES(\"time\")");
+    HDFql::execute(sstr("CREATE ATTRIBUTE dataset/dt AS DOUBLE VALUES(" << dt << ")"));
+    HDFql::execute(sstr("CREATE ATTRIBUTE dataset/θ AS DOUBLE VALUES(" << theta << ")"));
+    HDFql::execute(sstr("CREATE ATTRIBUTE dataset/μ AS DOUBLE VALUES(" << mu << ")"));
+    HDFql::execute(sstr("CREATE ATTRIBUTE dataset/σ AS DOUBLE VALUES(" << sigma << ")"));
+
+    HDFql::execute("CLOSE FILE");
 
     return 0;
 }
